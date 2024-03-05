5115094b85658c16216df1f8d417d2427e35da54
Omolemo Lethuloe <omolemo.lethuloe@shesha.io>
2024-03-05 19:41:35 +0200

    chore: login details initialization

--- adminportal/src/app/login/page.tsx
+++ adminportal/src/app/login/page.tsx
@@ -1,13 +1,18 @@
 "use client";
 
-import { EyeInvisibleOutlined, EyeTwoTone, LockOutlined, MailOutlined } from '@ant-design/icons';
-import { Button, Checkbox, Form, Input } from 'antd';
-import FormItem from 'antd/lib/form/FormItem';
-import Link from 'next/link';
-import React from 'react';
-import { useAuth, ValidationErrors } from '@shesha-io/reactjs';
-import { URL_FORGOT_PASSWORD } from '@/routes';
-import { LoginPageWrapper } from './wrapper';
+import {
+  EyeInvisibleOutlined,
+  EyeTwoTone,
+  LockOutlined,
+  MailOutlined,
+} from "@ant-design/icons";
+import { Button, Checkbox, Form, Input } from "antd";
+import FormItem from "antd/lib/form/FormItem";
+import Link from "next/link";
+import React from "react";
+import { useAuth, ValidationErrors } from "@shesha-io/reactjs";
+import { URL_FORGOT_PASSWORD } from "@/routes";
+import { LoginPageWrapper } from "./wrapper";
 
 interface ILoginForm {
   readonly userNameOrEmailAddress: string;
@@ -34,19 +39,37 @@ const Login = () => {
       heading="Welcome!"
       hint="Please enter your personal details in order to access your profile."
     >
-      <Form form={form} onFinish={handleLogin}>
+      <Form
+        form={form}
+        onFinish={handleLogin}
+        initialValues={{
+          userNameOrEmailAddress: "SheshaTutorial",
+          password: "SheshaTutorial01",
+        }}
+      >
         <ValidationErrors error={errorInfo} />
 
-        <FormItem name="userNameOrEmailAddress" help="This field is required" rules={[{ required: true }]}>
-          <Input prefix={<MailOutlined />} placeholder="Username" />
+        <FormItem
+          name="userNameOrEmailAddress"
+          help="This field is required"
+          rules={[{ required: true }]}
+        >
+          <Input prefix={<MailOutlined />} placeholder="Username" disabled />
         </FormItem>
 
-        <FormItem name="password" help="This field is required" rules={[{ required: true }]}>
+        <FormItem
+          name="password"
+          help="This field is required"
+          rules={[{ required: true }]}
+        >
           <Input.Password
             autoComplete="on"
             prefix={<LockOutlined />}
-            iconRender={(visible) => (visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />)}
+            iconRender={(visible) =>
+              visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />
+            }
             placeholder="Password"
+            disabled
           />
         </FormItem>
 
@@ -59,16 +82,12 @@ const Login = () => {
             loading={isLoggingInUser}
             size="large"
           >
-            {isLoggingInUser ? 'Signing in....' : 'Sign In'}
+            {isLoggingInUser ? "Signing in...." : "Sign In"}
           </Button>
         </FormItem>
 
         <div className="custom-form-item">
           <Checkbox>Remember me</Checkbox>
-
-          <Link href={URL_FORGOT_PASSWORD} className="login-form-forgot">
-            Forgot password
-          </Link>
         </div>
       </Form>
     </LoginPageWrapper>
