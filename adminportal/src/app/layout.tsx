5b2c13c2e2b1174040f27bf23f0c27af1985e9da
Omolemo Lethuloe <omolemo.lethuloe@shesha.io>
2024-03-25 15:29:44 +0200

    chore: googletag script

--- adminportal/src/app/layout.tsx
+++ adminportal/src/app/layout.tsx
@@ -30,6 +30,20 @@ export default function RootLayout({
           `,
           }}
         />
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-NKYL5VXNHY"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || []; 
+            function gtag(){dataLayer.push(arguments);} 
+            gtag('js', new Date()); 
+            gtag('config', 'G-NKYL5VXNHY'); 
+          `,
+          }}
+        />
         {/* End Google Tag Manager */}
       </head>
       <body>
